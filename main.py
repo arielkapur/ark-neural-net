25edb4cb6e3c59612f4fe6df26b299a8c6db1c09
ariel kapur <76942657+arielkapur@users.noreply.github.com>
2026-07-04 04:19:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from arkmath.vector import Vector
+from arkmath.vector import *
 
+u = Vector([1,2])
 v = Vector([1,2,3])
 w = Vector([4,5,6])
+A = Matrix([[1,2],[3,4],[5,6]])
 
-print(v+w)
-
+print(A @ u)  # Matrix-vector multiplication
