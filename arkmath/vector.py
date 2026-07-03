7d00603a2d7f446c3a45d5fb2938793cf3969ca6
ariel kapur <76942657+arielkapur@users.noreply.github.com>
2026-07-04 04:11:18 +1000

    Update vector.py

--- arkmath/vector.py
+++ arkmath/vector.py
@@ -1,4 +1,4 @@
-
+import math
 
 class Vector:
     def __init__(self, components):
@@ -9,8 +9,59 @@ class Vector:
 
     def __add__(self, other):
         result = []
-
         for i in range(len(self.components)):
             result.append(self.components[i] + other.components[i])
 
+        return Vector(result)
+
+    def dot(self, other):
+        if len(self.components) != len(other.components):
+            print("Error: Vectors must be of the same length to compute dot product.") #loopy bullshit incoming
+        dot = 0
+        for i in range(len(self.components)):
+            dot += self.components[i] * other.components[i]
+        return dot
+
+    def __mul__ (self, scalar):
+        result = []
+        for i in range(len(self.components)):
+            result.append(self.components[i] * scalar)
+        return Vector(result)
+
+    def __rmul__(self, scalar):
+        return self.__mul__(scalar)
+
+    def magnitude(self):
+        total = 0
+        for i in range(len(self.components)):
+            total += self.components[i] ** 2
+        return math.sqrt(total)
+
+    def normalise(self):
+        mag = self.magnitude
+        if mag == 0:
+            raise ValueError("Cannot normalise zero vector")
+        result = []
+        for i in range(len(self.components)):
+            result.append(self.components[i] / mag)
+        return Vector(result)
+
+    def cosine_similarity(self,other):
+        dot_product = self.dot(other)
+        magnitude_self = self.magnitude()
+        magnitude_other = other.magnitude()
+        if magnitude_self == 0 or magnitude_other == 0:
+            raise ValueError("Cannot compute cosine similarity with zero vector")
+        return dot_product/(magnitude_self * magnitude_other)
+    
+class Matrix:
+    def __init__(self, rows):
+        self.rows = rows
+    def __matmul__ (self, vector):
+        if len(self.rows[0]) != len(vector.components):
+            raise ValueError("Matrix columns must match vector size for multiplication.")
+        result = []
+        for row in self.rows:
+            dot_product = sum(row[i] * vector.components[i] for i in range(len(row)))
+            result.append(dot_product)
         return Vector(result)
